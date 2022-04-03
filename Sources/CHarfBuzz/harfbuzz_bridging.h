491f1aec0c3496d9743bee46bc4227782d7221a8
Rene Hexel <rhx@users.noreply.github.com>
2022-04-03 18:17:49 +1000

    Initial import

---
+++ Sources/CHarfBuzz/harfbuzz_bridging.h
@@ -0,0 +1,19 @@
+struct hb_blob_t {};
+struct hb_buffer_t {};
+struct hb_draw_funcs_t {};
+struct hb_face_t {};
+struct hb_font_t {};
+struct hb_font_funcs_t {};
+struct hb_link_t {};
+struct hb_map_t {};
+struct hb_object_t {};
+struct hb_set_t {};
+struct hb_shape_plan_t {};
+struct hb_subset_input_t {};
+struct hb_subset_plan_t {};
+struct hb_unicode_funcs_t {};
+
+#define GLIB_DISABLE_DEPRECATION_WARNINGS
+#include <sys/types.h>
+#include <harfbuzz/hb-glib.h>
+#include <harfbuzz/hb-gobject.h>
