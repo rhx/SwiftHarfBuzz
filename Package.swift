eeaa2f065062f3ef60b7894a1b0c60c3ff394968
Rene Hexel <r.hexel@griffith.edu.au>
2025-03-11 17:12:11 +1000

    Use main branch on dependencies.

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
             targets: ["HarfBuzz"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/rhx/gir2swift.git",    branch: "development"),
-        .package(url: "https://github.com/rhx/SwiftGObject.git", branch: "development"),
+        .package(url: "https://github.com/rhx/gir2swift.git",    branch: "main"),
+        .package(url: "https://github.com/rhx/SwiftGObject.git", branch: "main"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
@@ -27,7 +27,6 @@ let package = Package(
             name: "HarfBuzz",
             dependencies: [
                 "CHarfBuzz",
-                .product(name: "gir2swift", package: "gir2swift"),
                 .product(name: "GLibObject", package: "SwiftGObject"),
             ],
             swiftSettings: [
